e6fee9288c7046103cac9b394af324119475dad7
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-04 22:35:14 +0530

    Initial Commit

---
+++ variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("variable = A reusable container for a value.\n");
+    printf("Behaves as if it were the value it contains.\n");
+    printf("variable contains datatype.\n");
+    printf("Some of the datatype are given below.\n");
+    printf("int = whole numbers (4 bytes in modern systems.\nfloat = single-precision decimal number (4 bytes)");
+    printf("double = double-precision decimal number (8 bytes).\nchar = single character (1 byte)\n");
+    printf("char[] = array of characters (size varies).\nbool = true or false (1 byte)\n");
+
+
+    printf("These datatype are used in different vaya like.\n");
+
+    int x ;
+    x = 100;
+    float y = 100.8;
+    printf("The value of x : %d\n",x);
+    printf("The value of x : %f\n",y);
+
+
+    return 0;
+}
