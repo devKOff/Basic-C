05624ad1087a9ab934066dbdc41b128e6cae9726
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-05 10:48:25 +0530

    first commit

---
+++ arithmeticOperation.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+
+// arithmetic operators + - * / % ++ --
+
+    int x = 10;
+    int y = 2;
+    int p,q,r,s,t;
+
+    p = x + y;
+    q = x - y;
+    r = x * y;
+    s = x / y;
+    t = x % 2;
+
+// x++ increment
+// x-- decrement
+
+// augmented assignment operators
+
+    printf("%d\n", x);
+    printf("%d\n", y);
+    printf("%d\n", p);
+    printf("%d\n", q);
+    printf("%d\n", r);
+    printf("%d\n", s);
+    printf("%d\n", t);
+    printf("%d\n", x+x);
+
+
+    return 0;
+
+}
