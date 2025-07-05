8013a02623a0e8a147b44342aa1cc2c90054ebb2
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-05 10:47:26 +0530

    first commit

---
+++ inputMethod.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+    int main() {
+        int x;
+        printf("Enter the value of x = ");
+        scanf("%d", &x);
+        printf("The value of x is %d\n", x);
+
+        char operator;
+        printf("Enter the operator : ");
+        scanf(" %c", &operator);
+        printf("The operator is %c",operator);
+
+        return 0;
+    }
